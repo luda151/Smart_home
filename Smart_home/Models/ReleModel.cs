078766e193af620ee69fa03d05851ddc6c02fcb7
ludek.bulicek <45243808+luda151@users.noreply.github.com>
2020-02-17 17:46:51 +0100

    zalozeni

---
+++ Smart_home/Models/ReleModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Smart_home.Models
+{
+    public class ReleModel
+    {
+        int id { get; set; }
+    }
+}

60ce0162bd69f53217c569725740afcc5ff9ddd3
ludek.bulicek <45243808+luda151@users.noreply.github.com>
2020-02-19 16:08:53 +0100

    update

--- Smart_home/wwwroot/js/site.js
+++ Smart_home/wwwroot/js/site.js
@@ -13,3 +13,11 @@
         text.style.display = "none";
  }
 }
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function () {
+    output.innerHTML = this.value;
+}

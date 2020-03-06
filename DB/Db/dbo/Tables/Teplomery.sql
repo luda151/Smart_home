ab19072cf25440f370957c19a524aece87399aa1
luda151 <ludek151@seznam.cz>
2020-03-06 22:23:12 +0100

    update

---
+++ DB/Db/dbo/Tables/Teplomery.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Teplomery] (
+    [Id]       INT        NOT NULL,
+    [Nazev]    NVARCHAR(50) NULL,
+    [Umisteni] NVARCHAR(50) NULL,
+    [Aktivni]  TINYINT NULL,
+    [Regulace] TINYINT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ DB/Db/dbo/Tables/Teploty.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Teploty] (
+    [Id]       INT        NOT NULL,
+    [Teplomer] NVARCHAR(50) NULL,
+    [Date]     DATE NULL,
+    [Hodnota]  INT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+

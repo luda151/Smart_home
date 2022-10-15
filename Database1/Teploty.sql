2969fc334b4e36cf45dd3726cd95f6e89bd0a4b9
luda151 <ludek151@seznam.cz>
2022-10-15 12:49:44 +0200

    xx

--- Database1/Teploty.sql
+++ Database1/Teploty.sql
@@ -1,7 +1,7 @@
-﻿CREATE TABLE [dbo].[Teploty]
+﻿CREATE TABLE Teploty 
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [teplota] NVARCHAR(50) NULL, 
-    [vlhkost] NVARCHAR(50) NULL, 
-    [cas] NVARCHAR(50) NULL
+	 Id  INT NOT NULL PRIMARY KEY, 
+     teplota  NVARCHAR(50) NULL, 
+     vlhkost  NVARCHAR(50) NULL, 
+     cas  NVARCHAR(50) NULL 
 )
--- Database1/Termostat.sql
+++ Database1/Termostat.sql
@@ -1,13 +1,13 @@
-﻿CREATE TABLE [dbo].[Termostat]
+﻿CREATE TABLE Termostat 
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [IdTeplomeru] NCHAR(10) NULL, 
-    [Mistnost] NCHAR(10) NULL, 
-    [Umisteni] NCHAR(10) NULL, 
-    [ZadanaTeplota] NCHAR(10) NULL, 
-    [NastavenaTeplota] NCHAR(10) NULL, 
-    [PosledniTeplota] NCHAR(10) NULL, 
-    [NovaTeplota] NCHAR(10) NULL, 
-    [Date] NCHAR(10) NULL, 
-    [Teplot] NCHAR(10) NULL
+	 Id  INT NOT NULL PRIMARY KEY, 
+     IdTeplomeru  NCHAR(10) NULL, 
+     Mistnost  NCHAR(10) NULL, 
+     Umisteni  NCHAR(10) NULL, 
+     ZadanaTeplota  NCHAR(10) NULL, 
+     NastavenaTeplota  NCHAR(10) NULL, 
+     PosledniTeplota  NCHAR(10) NULL, 
+     NovaTeplota  NCHAR(10) NULL, 
+     Date  NCHAR(10) NULL, 
+     Teplot  NCHAR(10) NULL
 )
